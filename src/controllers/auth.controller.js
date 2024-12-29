a84e83e2c7dd7582e544650a9ee15e9a054fd475
R7Marl <lukaelcapo2017@gmail.com>
2024-12-29 19:47:52 -0300

    fix: vulnerability in creating account

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -117,7 +117,7 @@ export const register = async (req, res) => {
             name,
             email,
             password: hashedPassword,
-            role: role || 'superadmin'
+            role: role || 'user'
         });
 
         const token = generateToken(user);
