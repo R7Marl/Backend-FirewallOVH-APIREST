29c70c9d53ddf2d4855e69e8556b79e16e4d00b1
R7Marl <lukaelcapo2017@gmail.com>
2025-02-01 21:22:35 -0300

    feat: AI integrated

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,8 @@
+import AIRouter from "./ai/ai.routes.js";
 import authRouter from "./auth/auth.routes.js";
 import router from "./servers/firewall.routes.js"
 export default (app) => {
   app.use('/v1/servers', router);
   app.use('/v1/auth', authRouter);
+  app.use('/v1/ai', AIRouter);
 }
