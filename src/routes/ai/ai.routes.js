ef4f5b6942fd2f216d9bfa3cc2f6a1e8acc2272e
R7Marl <lukaelcapo2017@gmail.com>
2025-02-12 16:41:18 -0300

    deleted failed dist

--- src/routes/ai/ai.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var auth_middleware_1 = require("../../middleware/auth.middleware");
-var ai_controller_1 = require("../../controllers/ai.controller");
-var AIRouter = (0, express_1.Router)();
-AIRouter.get("/getAISuggestion", auth_middleware_1.authMiddleware, ai_controller_1.getAISuggestion);
-exports.default = AIRouter;
